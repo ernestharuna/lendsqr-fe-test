20a399d901ee51fdebd3fd1ec1161f9f65b1100a
Ernest Haruna <ernestharuna1@gmail.com>
2024-08-06 20:51:50 +0100

    Feat: Created Login Page - Configured AvenirNext font - Configured app Icons as SVG Props

--- src/components/layouts/root.tsx
+++ src/components/layouts/root.tsx
@@ -1,9 +1,20 @@
 import { Outlet } from "react-router-dom";
+import { AppLogo, AppName } from "../../assets/icons/logos";
 
 export default function Root() {
   return (
     <div>
-      Root
+      <nav>
+        <div>
+          <div className="icons">
+            <AppLogo /> <AppName />
+          </div>
+        </div>
+
+        <div>
+          <a href="#" className="d-block">Docs</a>
+        </div>
+      </nav>
       <Outlet />
     </div>
   )
--- src/components/routes/login.tsx
+++ src/components/routes/login.tsx
@@ -1,5 +1,41 @@
+import { AppLogo, AppName, PabloSign } from "../../assets/icons/logos";
+
 export default function Login() {
   return (
-    <div>Login</div>
+    <div className="font-regular" id="login-page">
+      <header className="container">
+        <div className="icons">
+          <AppLogo /> <AppName />
+        </div>
+      </header>
+
+      <main className="container">
+        <div className="flex items-center justify-between">
+          <section id="left-side">
+            <PabloSign width={"600px"} />
+          </section>
+
+          <section className="form-container" id="right-side">
+            <form action="#" className="container">
+              <h1 className="font-bold mb-0 mt-0">Welcome!</h1>
+              <p className="mb-13 mt-4 fs-5 text-secondary">Enter details to login.</p>
+              <div>
+                <input type="email" className="input-field w-100" placeholder="Email" />
+              </div>
+              <div>
+                <input type="password" className="input-field w-100" placeholder="Password" />
+              </div>
+              <p className="text-primary uppercase mb-2 text-small mb-10 font-semibold">
+                forgot password?
+              </p>
+
+              <button className="w-100">
+                LOG IN
+              </button>
+            </form>
+          </section>
+        </div>
+      </main>
+    </div>
   )
 }
