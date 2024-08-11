49a04b890d518ed44fc03c1b97b14f8a418be7f9
Ernest Haruna <ernestharuna1@gmail.com>
2024-08-11 13:35:36 +0100

    Bugfix: Made login mobile responsive

--- src/components/routes/login.tsx
+++ src/components/routes/login.tsx
@@ -1,41 +1,57 @@
+import { useState } from "react";
 import { AppLogo, AppName, PabloSign } from "../../assets/icons/logos";
 
 export default function Login() {
+
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
   return (
-    <div className="font-regular" id="login-page">
-      <header className="container">
+    <div className="avenir" id="login-page">
+      <header className="">
         <div className="icons">
           <AppLogo /> <AppName />
         </div>
       </header>
 
-      <main className="container">
-        <div className="flex items-center justify-between">
-          <section id="left-side">
-            <PabloSign width={"600px"} />
-          </section>
+      {/* <main className=""> */}
+      <div className="login-wrapper">
+        <section className="login-image-container">
+          <PabloSign className="login-image" />
+        </section>
 
-          <section className="form-container" id="right-side">
-            <form action="#" className="container">
-              <h1 className="font-bold mb-0 mt-0">Welcome!</h1>
-              <p className="mb-13 mt-4 fs-5 text-gray">Enter details to login.</p>
-              <div>
-                <input type="email" className="input-field w-100" placeholder="Email" />
-              </div>
-              <div>
-                <input type="password" className="input-field w-100" placeholder="Password" />
-              </div>
-              <p className="text-primary uppercase mb-2 text-small mb-10 font-semibold">
-                forgot password?
-              </p>
+        <section className="form-container">
+          <form action="/" method="get">
+            <h1 className="font-bold mb-0 mt-0">Welcome!</h1>
+            <p className="mb-13 mt-4 fs-5 text-gray">Enter details to login.</p>
+            <div>
+              <input type="email" className="input-field w-100" placeholder="Email" />
+            </div>
 
-              <button className="w-100">
-                LOG IN
+            <div className="login-password-field-wrapper">
+              <input className="login-password-field"
+                type={passwordVisible ? 'text' : 'password'}
+                placeholder="Password"
+              />
+              <button className="login-password-visibility"
+                type="button" onClick={togglePasswordVisibility}
+              >
+                {passwordVisible ? 'HIDE' : 'SHOW'}
               </button>
-            </form>
-          </section>
-        </div>
-      </main>
+            </div>
+            <p className="text-primary uppercase text-small mb-10 mt-7 font-semibold">
+              forgot password?
+            </p>
+
+            <button className="w-100 login-submit">
+              LOG IN
+            </button>
+          </form>
+        </section>
+      </div>
+      {/* </main> */}
     </div>
   )
 }
